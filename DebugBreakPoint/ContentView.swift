9cb6f93834edf5cb9164cf495596290373244e54
Mickael MAS <mickael.mas@appiwedia.com>
2020-01-09 16:03:23 +0100

    Initial Commit

---
+++ DebugBreakPoint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DebugBreakPoint
+//
+//  Created by Mickael Mas on 09/01/2020.
+//  Copyright © 2020 APPIWEDIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
